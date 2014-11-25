0f2e4e2489ef7c0ad974e6c31f8ca4d3f706bf52
mbeckius@gmail.com <mbeckius@gmail.com>
2014-11-24 23:39:12 -0500

    added world

--- MoreGitFun/Program.cs
+++ MoreGitFun/Program.cs
@@ -10,7 +10,7 @@ namespace MoreGitFun
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
     }
