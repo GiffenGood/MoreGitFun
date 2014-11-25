911f2e8c9b7b2b0cfb40bf6c059c0331a9e87967
mbeckius@gmail.com <mbeckius@gmail.com>
2014-11-24 23:27:34 -0500

    1st checkin

---
+++ MoreGitFun/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreGitFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
